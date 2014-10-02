4907c6a024cb3f24b08f0bb2f6d52ac8191ba6b2
David Branner <brannerchinese@gmail.com>
2014-10-02 11:33:09 -0400

    upload downloads from today

--- run_stock_requests.sh
+++ run_stock_requests.sh
@@ -4,8 +4,13 @@
 # 20140927
 
 cd /home/dpb/github_public/stockscrape
-source v_env3/bin/activate
+touch _first
+source /home/dpb/github_public/stockscrape/v_env3/bin/activate
+touch _second
 python /home/dpb/github_public/stockscrape/headline_to_db.py
+touch _third
 git add -A /home/dpb/github_public/stockscrape
+touch _fourth
 git commit -m 'upload downloads from today'
+touch _ fifth
 
