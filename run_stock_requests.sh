2f11f4688f1f6956f643d1db5ac5fdd911077ee1
David Branner <brannerchinese@gmail.com>
2014-10-09 07:24:10 -0400

    revise run_stock_requests.sh

--- run_stock_requests.sh
+++ run_stock_requests.sh
@@ -1,16 +1,11 @@
 # run_stock_requests.sh
 # Run stockscrape's `headline_to_db.py` within the master branch.
 # David Prager Branner
-# 20140927
+# 20141009
 
 cd /home/dpb/github_public/stockscrape
-touch _first
 source /home/dpb/github_public/stockscrape/v_env3/bin/activate
-touch _second
 python /home/dpb/github_public/stockscrape/headline_to_db.py
-touch _third
 git add -A /home/dpb/github_public/stockscrape
-touch _fourth
 git commit -m 'upload downloads from today'
-touch _fifth
 
